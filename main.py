b979c7c8e5df018b32c533033a6edfdfbb65f5f0
GaryRaubenheimer <71284910+GaryRaubenheimer@users.noreply.github.com>
2020-10-04 20:57:56 +0200

    Add files via upload
    Added tkinter app and image loader to main

--- main.py
+++ main.py
@@ -5,11 +5,15 @@
 # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 # Libraries -> Tkinter, PIL, os, sys, glob, shutil
 
-import os
+import os, sys, shutil
 
-ImageList = []                                              # empty Image list
-os.chdir(r'C:\Users\Gary\Desktop\New folder')               # os.chdir -> change current dir
-WorkingDir = os.getcwd()                                    # os.getcwd() -> get current working dir
+from tkinter import *
+from PIL import ImageTk, Image
+
+ImageList = []  # empty Image list
+os.chdir(r'C:\Users\Gary\Desktop\New folder')  # os.chdir -> change current dir
+WorkingDir = os.getcwd()  # os.getcwd() -> get current working dir
+
 
 def GetImageList():
     # get image files from working dir and put images in list
@@ -20,8 +24,33 @@ def GetImageList():
 
 
 def test():
-   # Display image on screen with in Tkinter
-    
+    # Display image on screen with in Tkinter
+
+    path = WorkingDir + '\\' + ImageList[10]
+
+    # create root app and window size
+    root = Tk()
+    width = root.winfo_screenwidth()-100
+    height = root.winfo_screenheight()-100
+    # root.overrideredirect(True)              #fullscreen
+    root.geometry(f'{width}x{height}')
+
+    # resize image
+    # originalImage = Image.open(path)
+    # reWidth = int(originalImage.size[0]/width*originalImage.size[0])
+    # reHeight = int(originalImage.size[1]/height*originalImage.size[1])
+    # new_width = root.width
+    # new_height = root.height
+    # resizeImage = originalImage.resize(width, height)
+    originalImage = Image.open(path)
+    resizeImage = originalImage.resize((width, height), Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(resizeImage)
+
+    panel = Label(root, image=img)
+    panel.pack(side="bottom", fill="both", expand="yes")
+    root.mainloop()
+
 
 if __name__ == '__main__':
+    GetImageList()
     test()
