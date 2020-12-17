2ec1b7749fa409b32c4e499bf85bb3814da6c9cd
GaryRaubenheimer <71284910+GaryRaubenheimer@users.noreply.github.com>
2020-12-17 12:08:16 +0200

    added patch 2
    the selection goes to the first image in the list after moving then delete the image in the list
    moving image only moves to the recently added button's folder

--- main.py
+++ main.py
@@ -15,9 +15,7 @@ from PIL import ImageTk, Image
 ImageList = []  # empty Image list
 ImageExtList = [".jpg", ".jpeg", ".bmp", ".gif", "png"]
 path = None
-
-os.chdir(r'C:\Users\Gary\Desktop\New folder')  # os.chdir -> change current dir
-WorkingDir = os.getcwd()  # os.getcwd() -> get current working dir
+# WorkingDir = ""
 
 
 def GetImageList():
@@ -29,8 +27,16 @@ def GetImageList():
             root.imageListbox.insert(index, f_name + f_ext)
 
 
+def LoadDirectory():
+    print("Load directory")
+    os.chdir(root.Working_Dir.get())  # os.chdir -> change current dir
+    # WorkingDir = os.getcwd()  # os.getcwd() -> get current working dir
+    GetImageList()
+    root.update()
+
+
 def GetImagePath(ImageName):
-    Path = WorkingDir + '\\' + ImageName
+    Path = os.getcwd() + '\\' + ImageName
     return Path
 
 
@@ -72,37 +78,43 @@ def changeImg(Path):
     root.panel.image = photo
 
 
-def on_click_listbox(event):
+def listboxChange(event):
     ImageChanged()
 
 
 def ImageChanged():
     try:
-        Selection = root.imageListbox.curselection()
-        if Selection != root.imageSelection:
-            newPath = GetImagePath(str(root.imageListbox.get(root.imageListbox.curselection())))
-            changeImg(newPath)
-            root.imageSelection = Selection
-            print("item changed")
-        else:
-            print("asdf")
+        newPath = GetImagePath(str(root.imageListbox.get(root.imageListbox.curselection())))
+        changeImg(newPath)
+        print("item changed")
     except TclError:
-        print("select item")
+        print("please select item")
 
 
-def MoveImage():
+def MoveImage(Index):
     print("move")
     OldImgPath = GetImagePath(str(root.imageListbox.get(root.imageListbox.curselection())))
-    NewImgPath = os.path.join(WorkingDir, root.folderButton['text'])
+    '''
+    # check button
+    print(Index, folderButtonList)
+    button = folderButtonList[Index]
+    buttonText = button['text']
+    '''
+    NewImgPath = os.path.join(os.getcwd(), root.folderButton['text'])
     shutil.move(OldImgPath, NewImgPath)
+    print(NewImgPath, root.folderButton)
+    # delete from listbox and select new item
+    root.imageListbox.delete(root.imageListbox.get(0, "end").index(str(root.imageListbox.get(root.imageListbox.curselection()))))
+    root.imageListbox.select_set(0)  # This only sets focus on the first item.
+    root.imageListbox.event_generate("<<ListboxSelect>>")
+    ImageChanged()
 
 
 def CreateNewFolder(FolderName):
     # Path
-    NewFolderPath = os.path.join(WorkingDir, FolderName)
+    NewFolderPath = os.path.join(os.getcwd(), FolderName)
 
     # Create the directory
-    # 'Nikhil'
     try:
         os.makedirs(NewFolderPath, exist_ok=True)
         print("Directory '%s' created successfully" % FolderName)
@@ -111,11 +123,11 @@ def CreateNewFolder(FolderName):
 
 
 def AddButton():
-
     if root.folderName.get() not in folderButtonList:
-        folderButtonList.append(root.folderName.get())
-        root.folderButton = Button(root.frame2, text=root.folderName.get(), command=MoveImage)
-        root.folderButton.pack(side="bottom", fill="both", expand="yes")
+        folderButtonList.append(root.folderButton)
+        root.folderButton = Button(root.frame2, text=root.folderName.get(), command=lambda: MoveImage(root.buttonIndex))
+        root.buttonIndex = root.buttonIndex + 1
+        root.folderButton.pack(side="top", fill="both", expand="no")
         CreateNewFolder(root.folderName.get())
 
 
@@ -128,6 +140,7 @@ class ImageOrganizer(tk.Frame):
     def initialize_user_interface(self):
         self.parent.title("Image Organizer")
         self.parent.geometry(f'{width}x{height}')
+        self.parent.resizable(width=0, height=0)
         '''
         # root.overrideredirect(True)              #fullscreen
         # root.rowconfigure(0, weight=1)
@@ -147,26 +160,32 @@ class ImageOrganizer(tk.Frame):
         root.frame1.grid(row=0, column=0, sticky="nsew")
         root.frame2.grid(row=1, column=0, sticky="nsew")
         root.frame_img.grid(row=0, column=1, rowspan=2, sticky="nsew")
+        # frames resize automatically with widget root.frame1.propagate(0) disables this
+        root.frame1.propagate(0)
+        root.frame2.propagate(0)
+        root.frame_img.propagate(0)
 
         # create image panel in image frame
         root.panel = tk.Label(root.frame_img, text="IMAGE")
         root.panel.pack(side="bottom", fill="both", expand="yes")
 
         # create widgets in frame1
-        root.WorkingDir = tk.Entry(root.frame1, text="Working Directory")
-        root.WorkingDir.pack(side="top", fill="both", expand="yes")
-        root.sb1 = Scrollbar(root.frame1)
-        root.sb1.pack(side="right", fill="both", expand="yes")
-        root.imageListbox = Listbox(root.frame1, yscrollcommand=root.sb1.set)
+        root.Working_Dir = tk.Entry(root.frame1, text="Working Directory")
+        root.Working_Dir.pack(side="top", fill="both", expand="no")
+        root.LoadDirButton = Button(root.frame1, text="Load Directory", command=LoadDirectory)
+        root.LoadDirButton.pack(side="top", expand="no")
+        root.sb1 = Scrollbar(root.frame1, orient="vertical")
+        root.sb1.pack(side="right", fill="both", expand="no")
+        root.imageListbox = Listbox(root.frame1, yscrollcommand=root.sb1.set, selectmode='browse')
         root.imageListbox.pack(side="bottom", fill="both", expand="yes")
-        root.imageSelection = None
         root.sb1.config(command=root.imageListbox.yview)
 
         # create widgets in frame 2
         root.folderName = tk.Entry(root.frame2, text="add folder")
-        root.folderName.pack(side="top", fill="both", expand="yes")
+        root.folderName.pack(side="top", fill="both", expand="no")
         root.folderButton = Button(root.frame2, text="+", command=AddButton)
-        root.folderButton.pack(side="bottom", fill="both", expand="yes")
+        root.folderButton.pack(side="bottom", fill="both", expand="no")
+        root.buttonIndex = 0
 
 
 if __name__ == '__main__':
@@ -178,9 +197,8 @@ if __name__ == '__main__':
     folderButtonList = []
 
     app = ImageOrganizer(root)
-    GetImageList()
 
     while TRUE:
         # use left mouse click on a list item to display selection
-        root.imageListbox.bind('<ButtonRelease-1>', on_click_listbox)
+        root.imageListbox.bind('<<ListboxSelect>>', listboxChange)
         root.mainloop()
